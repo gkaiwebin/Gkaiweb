1680c89aff2e488c0c239f496a9da216bc4c8c1c
gkaiwebin <gkaiweb.in@gmail.com>
2026-06-03 00:12:58 +0530

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://127.0.0.1:27017/gkaiweb")
-.then(() => {
-    console.log("MongoDB Connected");
-})
-.catch((err) => {
-    console.log("Connection Error:", err);
-});
